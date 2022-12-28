edab318a0b598f3df8958a6d4ba12c0c44fd9704
wamomo-m <wamomosan@gmail.com>
2022-12-28 22:10:02 +0900

    :seedling: spring initializr

---
+++ src/main/kotlin/mappertest/MapperTestApplication.kt
@@ -0,0 +1,11 @@
+package mappertest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MapperTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<MapperTestApplication>(*args)
+}
---
+++ src/test/kotlin/mappertest/MapperTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package mappertest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MapperTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
